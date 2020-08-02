a6b9348deec42a15c40aa9da885cfefe517c2fed
estebancabreraa <esteban.cabrera.a@gmail.com>
2020-08-02 00:06:02 -0600

    termine la clase emisor

--- emisor.py
+++ emisor.py
@@ -1,23 +1,27 @@
 import socket
 import binascii
-import bitarray
+from bitarray import bitarray
 
 class Emisor:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((socket.gethostname(), 1234))
-    s.listen(3)
 
     def __init__(self, name):
         self.name = name
+        self.sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sckt.bind((socket.gethostname(), 1234))
+        self.sckt.listen(3)
         print("Se creo un emisor.")
+    
+    def enviar_objeto(self, cadena__bitarray):
+        clientsocket, address = self.sckt.accept()
+        clientsocket.send(cadena__bitarray)
 
-    @staticmethod
-    def enviar_cadena_segura(msg):
+    def enviar_cadena_segura(self, msg):
         cadena__binascii = bin(int.from_bytes(msg.encode(), 'big'))
-        print (cadena__binascii)
+        cadena__bitarray = bitarray(cadena__binascii[2:])
+        self.enviar_objeto(cadena__bitarray)
 
     def enviar_cadena(self):
-        self.enviar_cadena_segura("hola como estas")
+        self.enviar_cadena_segura("hola que tal")
 
 
 em = Emisor("Esteban")
