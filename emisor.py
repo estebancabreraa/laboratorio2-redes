14b3412c1210a6b3313c604d2a5398ec7aa5ca21
estebancabreraa <esteban.cabrera.a@gmail.com>
2020-08-03 16:46:22 -0600

    Implementacion de ruido en la cadena

--- emisor.py
+++ emisor.py
@@ -1,6 +1,7 @@
 import socket
 import pickle
 import binascii
+from random import randrange
 from bitarray import bitarray
 
 class Emisor:
@@ -11,6 +12,13 @@ class Emisor:
         self.sckt.bind((socket.gethostname(), 1234))
         self.sckt.listen(3)
         print("Se creo un emisor.")
+
+    def cadena_con_ruido(self, cadena):
+        caracter = randrange(0, 2)
+        pos = randrange(0, len(cadena) - 1)
+        cadena = cadena[:pos] + str(caracter) + cadena[pos+1:]
+        return cadena
+        
     
     def enviar_objeto(self, data):
         clientsocket, address = self.sckt.accept()
@@ -18,7 +26,7 @@ class Emisor:
 
     def enviar_cadena_segura(self, msg):
         cadena__binascii = bin(int.from_bytes(msg.encode(), 'big'))
-        cadena__bitarray = bitarray(cadena__binascii[2:])
+        cadena__bitarray = bitarray(self.cadena_con_ruido(cadena__binascii[2:]))
         data = pickle.dumps(cadena__bitarray)
         self.enviar_objeto(data)
         #self.enviar_objeto(cadena__binascii)
@@ -29,3 +37,4 @@ class Emisor:
 
 em = Emisor("Esteban")
 em.enviar_cadena()
+
